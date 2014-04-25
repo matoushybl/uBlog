3e06040175764f59aa8da5a14a17f6a73655062b
Matouš Hýbl <hyblmatous@gmail.com>
2014-04-25 21:35:38 +0200

    Initial commit

---
+++ src/main/java/com/mat/hyb/ublog/MainActivity.java
@@ -0,0 +1,12 @@
+package com.mat.hyb.ublog;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+@OptionsMenu(R.menu.main)
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+}
