ad479577661f3b6f0f3c2eb9996bcd6cc9fd2451
Matouš Hýbl <hyblmatous@gmail.com>
2014-04-26 15:20:17 +0200

    Add deleteAll to preferences

--- src/main/java/com/mat/hyb/ublog/fragment/SettingsFragment.java
+++ src/main/java/com/mat/hyb/ublog/fragment/SettingsFragment.java
@@ -1,9 +1,15 @@
 package com.mat.hyb.ublog.fragment;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
-import org.androidannotations.annotations.EFragment;
+import com.mat.hyb.ublog.R;
+import com.mat.hyb.ublog.entity.Post;
+
+import org.brightify.torch.TorchService;
+
+import java.util.List;
 
 /**
  * Created by matous on 26.4.14 for uBlog.
@@ -13,6 +19,17 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        addPreferencesFromResource(R.xml.main_preferences);
+        Preference reset = findPreference("deleteAll");
+        if (reset != null) {
+            reset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    List<Post> posts = TorchService.torch().load().type(Post.class).list();
+                    TorchService.torch().delete().entities(posts);
+                    return true;
+                }
+            });
+        }
     }
 }
